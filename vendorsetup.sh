e3df99c91970c36af9c7d0d8437a64c97e0800a9
shezan-tech <ariekechield22@gmail.com>
2025-05-16 18:58:20 +0700

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,10 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_emerald.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_emerald-user \
-    omni_emerald-userdebug \
-    omni_emerald-eng
+add_lunch_combo omni_emerald-user
+add_lunch_combo omni_emerald-userdebug
+add_lunch_combo omni_emerald-eng
